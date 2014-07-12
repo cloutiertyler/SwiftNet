c26a1dd5300f0603f71e40f815e75cb9d89410c3
Tyler Cloutier <cloutiertyler@aol.com>
2014-07-12 01:04:36 -0400

    Created new dynamic framework and Xcode project for SwiftNet.

---
+++ SwiftNet/SwiftNet/SwiftNet.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNet.h
+//  SwiftNet
+//
+//  Created by Tyler Fleming Cloutier on 7/12/14.
+//  Copyright (c) 2014 Starfire, LP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftNet.
+FOUNDATION_EXPORT double SwiftNetVersionNumber;
+
+//! Project version string for SwiftNet.
+FOUNDATION_EXPORT const unsigned char SwiftNetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNet/PublicHeader.h>
+
+
